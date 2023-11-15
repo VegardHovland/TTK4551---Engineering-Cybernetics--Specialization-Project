f648ff809efe56d794f9a500c622b00e54e905b4
VegardHovland <78042301+VegardHovland@users.noreply.github.com>
2023-11-15 21:38:01 +0100

    Path excution, plotting and mission planning

--- autonomous_data_sampling_ws/src/mission_planning_execution/scripts/mission_planner.py
+++ autonomous_data_sampling_ws/src/mission_planning_execution/scripts/mission_planner.py
@@ -1,9 +1,44 @@
-
+#! /usr/bin/env python3
 import rospy
 import moveit_commander
 from geometry_msgs.msg import PoseStamped
-from moveit_msgs.msg import DisplayTrajectory
-
+from moveit_msgs.msg import DisplayTrajectory, RobotState
+import rospy
+import actionlib
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+from std_msgs.msg import Int32, Bool, String
+from sensor_msgs.msg import JointState, MultiDOFJointState
+from control_msgs.msg import FollowJointTrajectoryGoal 
+from control_msgs.msg import FollowJointTrajectoryActionGoal
+from std_msgs.msg import Float32MultiArray,Header
+import time
+from trajectory_msgs.msg import JointTrajectoryPoint, MultiDOFJointTrajectory, MultiDOFJointTrajectoryPoint
+from control_msgs.msg import (
+    FollowJointTrajectoryFeedback,
+    FollowJointTrajectoryResult,
+)
+#from trajectory_msgs.msg import (JointTrajectoryPoint)
+from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion, Transform, Twist
+from actionlib_msgs.msg import GoalStatusArray, GoalID, GoalStatus
+import math
+import tf
+#from joint_command_interface.action import FollowJointTrajectoryAction, MultiDOFJointTrajectoryAction 
+#from joint_command_interface.msg import      
+from moveit_msgs.msg import ExecuteTrajectoryActionGoal, DisplayTrajectory, PlanningScene
+from visualization_msgs.msg import Marker
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+from math import pi
+from std_msgs.msg import String
+from moveit_commander.conversions import pose_to_list
+import sys
 
 class QuadrotorCommander:
     def __init__(self):
@@ -11,17 +46,29 @@ class QuadrotorCommander:
         moveit_commander.roscpp_initialize(sys.argv)
         self.robot = moveit_commander.RobotCommander()
         self.scene = moveit_commander.PlanningSceneInterface()
-        self.group_name = "quadrotor"
+        self.group_name = "rmf_obelix_planning_group"
         self.move_group = moveit_commander.MoveGroupCommander(self.group_name)
-        self.display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory, queue_size=10)
-
-    def set_start_position(self, x, y, z):
+        self.display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path', DisplayTrajectory, queue_size=10)
+        self.pose = Pose()
+        #elf.move_group.set_planning_time(10)
+        #elf.move_group.set_workspace([-5, -5, -5, 5, 5, 5])
+        #elf.move_group.set_planner_id("RRTConnectkConfigDefault")
+        #elf.move_group.set_num_planning_attempts(10)
+        #self.move_group.set_max_velocity_scaling_factor(1.0)
+        #self.move_group.set_max_acceleration_scaling_factor(1.0)
+        #self.move_group.set_goal_tolerance(0.1) 
+        rospy.sleep(5)                                                            # init pos for joint states       
+        self.sub = rospy.Subscriber("/rmf_obelix/ground_truth/pose", Pose, callback=self.get_pose)
+        #/move_group/monitored_planning_scene
+        self.plansub = rospy.Subscriber("move_group/monitored_planning_scene", PlanningScene, callback=self.get_planning_scene)
+        self.planscene_init  = 0
+    def set_start_position(self):
         # Set the starting pose
         start_pose = PoseStamped()
         start_pose.header.frame_id = "world"
-        start_pose.pose.position.x = x
-        start_pose.pose.position.y = y
-        start_pose.pose.position.z = z
+        start_pose.pose.position.x = self.pose.position.x
+        start_pose.pose.position.y = self.pose.position.y
+        start_pose.pose.position.z = self.pose.position.z
         self.move_group.set_start_state_to_current_state()
         self.move_group.set_pose_target(start_pose)
         self.move_group.go(wait=True)
@@ -29,33 +76,132 @@ class QuadrotorCommander:
         self.move_group.clear_pose_targets()
 
     def go_to_waypoint(self, x, y, z):
+        # setstart state
+        if self.planscene_init == 0:
+            return
+        #print planining scene
+        #print(self.scene)
+        self.move_group.set_start_state_to_current_state()
         # Set the target pose
-        pose_target = PoseStamped()
-        pose_target.header.frame_id = "world"
-        pose_target.pose.position.x = x
-        pose_target.pose.position.y = y
-        pose_target.pose.position.z = z
+        pose_target = Pose()
+
+        #pose_target.header.frame_id = "world"
+        pose_target.position.x = x
+        pose_target.position.y = y
+        pose_target.position.z = z
+        pose_target.orientation.w = 1.0
         self.move_group.set_pose_target(pose_target)
 
-        # Plan and execute the trajectory
+        #self.move_group.set_start_state()
+        # Plan 
+        self.move_group.set_planning_time(10)
+        self.move_group.set_workspace([-5, 5, -5, 5, -5, 5])
         plan = self.move_group.go(wait=True)
-        self.move_group.stop()
-        self.move_group.clear_pose_targets()
+        #self.move_group.stop()
+        #self.move_group.clear_pose_targets()
+        
+        #Dsiplay trajectory
+        display_trajectory = DisplayTrajectory()
+        display_trajectory.trajectory_start = self.robot.get_current_state()
+        display_trajectory.trajectory.append(plan)
+        print(display_trajectory)
+        try:
+            self.display_trajectory_publisher.publish(display_trajectory)
+        except:
+            print("Error calculating trajectory")
+            pass
+
+
+
+
+
+    def get_pose(self, pose):
+        # Get pose
+        self.pose = pose
+        #print("Pose: {}".format(self.pose))
+    def get_planning_scene(self, scene):
+        self.planscene_init = 1
+        print("planning scene")
+        #print("Planning scene: {}".format(scene))
+        self.scene = scene
+    def debugMovegroup(self):
+        # We can get the name of the reference frame for this robot:
+        #planning_frame = self.group.get_planning_frame()
+        #print("============ Reference frame: %s" % planning_frame)
+
+        # We can get a list of all the groups in the robot:
+        group_names = self.robot.get_group_names()
+        print("============ Robot Groups:", group_names)
+
+        # Sometimes for debugging it is useful to print the entire state of the
+        # robot:
+        print("============ Printing robot state")
+        print(self.robot.get_current_state())
+        print("")
+    
+    
 
 if __name__ == '__main__':
     try:
         # Initialize the node
         rospy.init_node('quadrotor_commander', anonymous=True)
-
         # Create the MoveIt commander interface
         quadrotor_commander = QuadrotorCommander()
-        # Get robot position
+        # set planning time to 10s and workspace to 10x10x5m
+        #for i in range(5):
+        #    quadrotor_commander.move_group.set_planning_time(10)
+        #    quadrotor_commander.move_group.set_workspace([-5, -5, -5, 5, 5, 5])
+        #    quadrotor_commander.move_group.set_planner_id("RRTConnectkConfigDefault")
+        #    quadrotor_commander.move_group.set_num_planning_attempts(10)
+        #    quadrotor_commander.move_group.set_max_velocity_scaling_factor(1.0)
+        #    quadrotor_commander.move_group.set_max_acceleration_scaling_factor(1.0)
+        #    quadrotor_commander.move_group.set_goal_tolerance(0.1)
+        #    rospy.sleep(1)
+        while True:
+        #    print(quadrotor_commander.move_group.get_current_state())
+        #    current_robot_state = quadrotor_commander.move_group.get_current_state()
+        #    # Extract the joint positions from the MultiDOFJointState
+        #    joint_state_msg = JointState()
+        #    joint_state_msg.name = current_robot_state.joint_state.name
+        #    joint_state_msg.position = [
+        #        current_robot_state.multi_dof_joint_state.transforms[0].translation.x,
+        #        current_robot_state.multi_dof_joint_state.transforms[0].translation.y,
+        #        current_robot_state.multi_dof_joint_state.transforms[0].translation.z,
+        #        current_robot_state.multi_dof_joint_state.transforms[0].rotation.x,
+        #        current_robot_state.multi_dof_joint_state.transforms[0].rotation.y,
+        #        current_robot_state.multi_dof_joint_state.transforms[0].rotation.z,
+        #        current_robot_state.multi_dof_joint_state.transforms[0].rotation.w,
+        #        ]
+#
+        #    # Set the modified JointState as the start state
+        #    quadrotor_commander.move_group.set_start_state(joint_state_msg)
+#
+            quadrotor_commander.move_group.set_start_state(quadrotor_commander.move_group.get_current_state())
+            quadrotor_commander.move_group.set_start_state_to_current_state()
+            quadrotor_commander.go_to_waypoint(0, 0, 1)
+            rospy.sleep(2)
+        # Set waypoint list of points to visit
+        waypoint_list = [[1.0, 2.0, 3.0],
+                          [4.0, 5.0, 6.0],
+                          [7.0, 8.0, 9.0],
+                          [10.0, 11.0, 12.0],
+                          [13.0, 14.0, 15.0]]
         
+        #quadrotor_commander.debugMovegroup()
+        #while True:
+        #    quadrotor_commander.go_to_waypoint(0, 0, 1)
+        #    rospy.sleep(2)
+        # Get robot position
+        #rospy.Subscriber("/rmf_obelix/ground_truth/pose", Pose, callback=self.get_pose)
         # Set the starting position of the quadrotor
-        quadrotor_commander.set_start_position(0.0, 0.0, 0.0)
+        #while True:
+        #    for wpt in waypoint_list:
+        #        quadrotor_commander.go_to_waypoint(wpt[0], wpt[1], wpt[2])
+        #        rospy.sleep(2)
+        #quadrotor_commander.set_start_position()
 
         # Command the quadrotor to a waypoint
-        quadrotor_commander.go_to_waypoint(1.0, 2.0, 3.0)
+        #quadrotor_commander.go_to_waypoint(1.0, 2.0, 3.0)
 
     except rospy.ROSInterruptException:
         pass
--- autonomous_data_sampling_ws/src/mission_planning_execution/scripts/path_executor.py
+++ autonomous_data_sampling_ws/src/mission_planning_execution/scripts/path_executor.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
 import rospy
 import actionlib
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 from std_msgs.msg import Int32, Bool, String
 from sensor_msgs.msg import JointState
 from control_msgs.msg import FollowJointTrajectoryGoal 
@@ -21,6 +23,8 @@ import tf
 #from joint_command_interface.msg import      
 from moveit_msgs.msg import ExecuteTrajectoryActionGoal, DisplayTrajectory
 from visualization_msgs.msg import Marker
+import matplotlib.pyplot as plt
+import numpy as np
 #from service_proxies import *
 #from moveit import MoveGroupPythonInterface
 #from moveit import MoveGroupCommander
@@ -49,14 +53,19 @@ class TrajectoryActionController(object):
         # Load map /move_group/load_map
         # LoadMap = '/map path'
         # map = rospy.ServiceProxy('/move_group/load_map', LoadMap)
+
+        self.xDrone = []
+        self.yDrone = []
+        self.zDrone = []
         rospy.loginfo('Successful init')                                                            
         rospy.spin()                                                                                # Loop ros comunication
     def get_path(self, path):
         self.path = path
         rospy.loginfo("New path from planner {}".format(self.path.trajectory[0].multi_dof_joint_trajectory.points))
         self.path = self.path.trajectory[0].multi_dof_joint_trajectory.points   #Store planned waypoints
+        #self.matplotPath() # Plot 3d path using matplotlib
         # Visualize planned path in Rviz
-        self.visPath(self.path)
+        # self.visPath(self.path)
         # Matplot lib visualization
 
 
@@ -64,6 +73,10 @@ class TrajectoryActionController(object):
     def execute(self, goal): 
         rospy.loginfo("Executing callback for trajectory execution beep boop")
         rospy.sleep(1)
+        #Init list for storing drone position for plotting
+        self.xDrone = []
+        self.yDrone = []
+        self.zDrone = []
         #Loop through all points in trajectory
         for _point in self.path:
             _w =_point.transforms
@@ -87,22 +100,27 @@ class TrajectoryActionController(object):
             # Publish a few times - publishing just once might fail.
             self.pub.publish(trajectory_execute)
             #rospy.sleep(0.5)
+            #Add drone postion
+            self.xDrone.append(self.pose.position.x)
+            self.yDrone.append(self.pose.position.y)
+            self.zDrone.append(self.pose.position.z)
             #Wait until point is reached
             while not self.tol(_w):
-                rospy.sleep(0.1)             
-
-        # Reset counter for next trajecotry execution
-        #rospy.loginfo("Canceling planning in moveit allowing for replanning")
-        #answerCancel = GoalID()
-        #answerCancel.stamp = rospy.get_rostime()
-        #answerCancel.id = ""
-        #self.pubCancelAction.publish(answerCancel)
+                rospy.sleep(0.001)             
+        rospy.loginfo("Finished executing trajectory")
+        # Make plots
+        self.matplotPath() # Plot 3d path using matplotlib
     
     def get_pose(self, pose):
         self.pose = pose
 
     def tol(self, current):
         _pose = self.pose.position
+        #Update drone state for plotting
+        self.xDrone.append(_pose.x)
+        self.yDrone.append(_pose.y)
+        self.zDrone.append(_pose.z)
+        #Calculate distance to waypoint
         diff_x = (_pose.x - current.x) ** 2
         diff_y = (_pose.y - current.y) ** 2
         diff_z = (_pose.z - current.z) ** 2
@@ -113,7 +131,45 @@ class TrajectoryActionController(object):
         else:
             rospy.loginfo("Diff from waypoint is {}, waiting".format(_diff))
             return False
-        
+    def matplotPath(self):
+        _x = []
+        _y = []
+        _z = []
+        for _point in self.path:
+            _w =_point.transforms
+            _w = _w[0].translation
+            _x.append(_w.x)
+            _y.append(_w.y)
+            _z.append(_w.z)
+        X = np.array(_x)
+        Y = np.array(_y)
+        Z = np.array(_z)
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot(X, Y, Z, label = 'Planned path', ls='solid',color='blue')
+        # Mark the start and end points with different markers
+        ax.scatter(X[0], Y[0], Z[0], c='green', marker='o', s=100, label='Start Point')
+        ax.scatter(X[-1], Y[-1], Z[-1], c='red', marker='*', s=100, label='End Point')
+        # Set lables and title
+        ax.set_xlabel('X [m]]')
+        ax.set_ylabel('Y [m]]')
+        ax.set_zlabel('Z [m]]')
+        ax.set_title('Planned Path vs Quadrotor Position')
+        ax.grid(True)
+        # Plot drone position if traj executed
+        if(self.xDrone  and self.yDrone  and self.zDrone ):
+            X_drone = np.array(self.xDrone)
+            Y_drone = np.array(self.yDrone)
+            Z_drone = np.array(self.zDrone)
+            ax.scatter(X_drone, Y_drone,Z_drone, c='black', marker='x', s=1, label='Quadrotor Position')
+            #ax.plot(X_drone, Y_drone, Z_drone, 'ro', label='drone position')
+        ax.legend()    
+        plt.show()
+        # Plot self.x self.y and self.z in same plot, adjust to same length
+        #ax.plot(self.x, self.y, self.z)
+
+
+
     def visPath(self, path):
         marker = Marker()
         marker.header.frame_id = "/map"
